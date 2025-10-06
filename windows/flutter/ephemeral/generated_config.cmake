0652590925e820af9a38490699dca8c538f45f2e
Henrique-TO <144277732+Henrique-TO@users.noreply.github.com>
2025-10-06 11:41:22 -0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\MARKETING3\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\MARKETING3\\Desktop\\sistema_pedidos_flutter" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\MARKETING3\\flutter"
+  "PROJECT_DIR=C:\\Users\\MARKETING3\\Desktop\\sistema_pedidos_flutter"
+  "FLUTTER_ROOT=C:\\Users\\MARKETING3\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\MARKETING3\\Desktop\\sistema_pedidos_flutter\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\MARKETING3\\Desktop\\sistema_pedidos_flutter"
+  "FLUTTER_TARGET=lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzUuNQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049YWM0ZTc5OWQyMw==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049ZDNkNDVkY2YyNQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjI="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=true"
+  "PACKAGE_CONFIG=C:\\Users\\MARKETING3\\Desktop\\sistema_pedidos_flutter\\.dart_tool\\package_config.json"
+)
